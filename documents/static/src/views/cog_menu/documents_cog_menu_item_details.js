23167177c765f642214646e3a7b9010bfab4e379
cilufarag <cgarafulic@gmail.com>
2025-01-27 21:52:18 -0500

    initial commit ee-18

---
+++ documents/static/src/views/cog_menu/documents_cog_menu_item_details.js
@@ -0,0 +1,26 @@
+import { STATIC_COG_GROUP_ACTION_ADVANCED } from "./documents_cog_menu_group";
+import { DocumentsCogMenuItem } from "./documents_cog_menu_item";
+import { _t } from "@web/core/l10n/translation";
+
+export class DocumentsCogMenuItemDetails extends DocumentsCogMenuItem {
+    setup() {
+        this.icon = "fa-info-circle";
+        this.label = _t("Info & Tags");
+        super.setup();
+    }
+
+    async doActionOnFolder(folder) {
+        await this.env.documentsView.bus.trigger("documents-toggle-chatter");
+    }
+}
+
+export const documentsCogMenuItemDetails = {
+    Component: DocumentsCogMenuItemDetails,
+    groupNumber: STATIC_COG_GROUP_ACTION_ADVANCED,
+    isDisplayed: (env) =>
+        DocumentsCogMenuItem.isVisible(
+            env,
+            ({ documentService, folder }) =>
+                documentService.userIsInternal && typeof folder.id === "number"
+        ),
+};
